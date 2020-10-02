f4214fd8477f4742c385eb9f3fe627d564795229
Cain <71161077+TheEpicYeti@users.noreply.github.com>
2020-10-02 10:14:48 +0100

    Add files via upload

---
+++ xfce-script.sh
@@ -0,0 +1,1 @@
+pacman -S linux-zen linux-zen-headers dkms nvidia-dkms nvidia-utils nvidia-settings lib32-mesa lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader vulkan-radeon meson systemd dbus chromium krita libreoffice-fresh discord steam lutris obs-studio wine neofetch terminator --noconfirm
