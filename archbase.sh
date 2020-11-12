738a72988bbe28778bc3b6cd36a2b367c01ae4a7
Cain <71161077+TheEpicYeti@users.noreply.github.com>
2020-11-12 13:03:37 +0000

    Update archbase.sh

--- archbase.sh
+++ archbase.sh
@@ -1,1 +1,1 @@
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog mtools dosfstools base-devel linux-headers pulseaudio alsa-utils reflector xdg-utils xdg-user-dirs
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog mtools dosfstools base-devel linux-headers pulseaudio alsa-utils reflector xdg-utils xdg-user-dirs --noconfirm
