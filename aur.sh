e84cc54d2d07c795005e0d1902f1585c599a369d
Cain <71161077+TheEpicYeti@users.noreply.github.com>
2020-11-20 20:33:23 +0000

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yay -S spotify slack-desktop ocs-url dropbox timeshift wd719x-firmware aic94xx-firmware --noconfirm
+yay -S spotify slack-desktop ocs-url dropbox timeshift wd719x-firmware aic94xx-firmware timeshift-autosnap --noconfirm
