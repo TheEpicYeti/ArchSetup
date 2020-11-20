5cf4927aac2df80e42d0857dbad4654d2aeedd4e
Cain <71161077+TheEpicYeti@users.noreply.github.com>
2020-11-20 20:15:08 +0000

    Update importkeys.sh

--- importkeys.sh
+++ importkeys.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd ~ curl -sS https://download.spotify.com/debian/pubkey.gpg | gpg --import -
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+cd ~ 
+curl -sS https://download.spotify.com/debian/pubkey.gpg | gpg --import -
+cd ~ 
+wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
